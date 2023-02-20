7e111f1248b39033937fcb16012094f455e4d7e5
bharatrai53 <bharatrai2016@gmail.com>
2023-02-19 17:21:29 -0800

    initial commit, file structure

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,90 @@
+const router = require("express").Router();
+const { User, Post, Comment, Like } = require("../models");
+const withAuth = require("../utils/auth");
+
+// RENDER HOMEPAGE with Post and Comment data (withAuth to redirect to "/login")
+router.get("/", withAuth, async (req, res) => {
+  try {
+    // --- POST DATA ---
+    // Get posts and comments with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Comment,
+          include: User,
+        },
+      ],
+    });
+
+    // Serialize post data so template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // -- RENDER HOMEPAGE --
+    // Pass serialized data and session flag into template and render Homepage
+    res.render("homepage", { posts });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// LOGIN ROUTE (if user logged in, redirect to home "/")
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+// USER PROFILE ROUTE
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      // include Post data for that user
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+    // Render profile page with user data
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST ROUTE for specific post id
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+    // Serialize post data so template can read it
+    const post = postData.get({ plain: true });
+
+    res.render("post", {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// MODULE EXPORT
+module.exports = router;
---
+++ public/js/homePage.js
@@ -0,0 +1,120 @@
+// FORM HANDLERS
+// Create Post Form Handler
+const postHandler = async (event) => {
+    event.preventDefault();
+  
+    const content = document.querySelector("#userPost").value.trim();
+  
+    if (content) {
+      // Send a POST request to the API endpoint
+      const response = await fetch("/api/post", {
+        method: "POST",
+        body: JSON.stringify({ content }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace("/");
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  // Create Comment Form Handler
+  const commentHandler = async (event) => {
+    event.preventDefault();
+    // Get user text and "data-postid" from closest parent div (post div)
+    const content = event.target[0].value;
+    const post_id = event.target.closest(".post").dataset.postid;
+  
+    if (content && post_id) {
+      // Send a POST request to the API endpoint
+      const response = await fetch("/api/comment", {
+        method: "POST",
+        body: JSON.stringify({ content, post_id }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace("/");
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  // Delete Post Form Handler
+  const postDeleteHandler = async (event) => {
+    event.preventDefault();
+    // "data-postid" from closest parent div (post div)
+    const post_id = event.target.closest(".post").dataset.postid;
+  
+    if (post_id) {
+      // Send a DELETE request to the API endpoint
+      const response = await fetch(`/api/post/${post_id}`, {
+        method: "DELETE",
+        body: JSON.stringify({ post_id }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace("/");
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  // Delete Comment Form Handler
+  const commentDeleteHandler = async (event) => {
+    event.preventDefault();
+    // "data-postid" from closest parent div (post div)
+    const comment_id = event.target.closest(".comment").dataset.commentid;
+  
+    if (comment_id) {
+      // Send a DELETE request to the API endpoint
+      const response = await fetch(`/api/comment/${comment_id}`, {
+        method: "DELETE",
+        body: JSON.stringify({ comment_id }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace("/");
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  // EVENT LISTENERS
+  
+  // Create Post Event Listener
+  document.querySelector(".post-form").addEventListener("submit", postHandler);
+  
+  // Create Comment Event Listener for all instances of comment-form
+  let commentSubmit = document.querySelectorAll(".comment-form");
+  
+  commentSubmit.forEach((btn) => {
+    btn.addEventListener("submit", commentHandler);
+  });
+  
+  // Delete Post Event Listener for all instances of post-delete-form
+  let postDeleteSubmit = document.querySelectorAll(".post-delete-form");
+  
+  postDeleteSubmit.forEach((btn) => {
+    btn.addEventListener("submit", postDeleteHandler);
+  });
+  
+  // Delete Comment Event Listener for all instances of comment-delete-form
+  let commentDeleteSubmit = document.querySelectorAll(".comment-delete-form");
+  
+  commentDeleteSubmit.forEach((btn) => {
+    btn.addEventListener("submit", commentDeleteHandler);
+  });
+  
